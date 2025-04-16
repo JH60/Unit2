6a2ba10b481cb9ccacf2471dde2925f9792630e9
Jaden Heimbach <heimbachj@roverkids.org>
2025-04-16 13:35:08 -0400

    Completed 2.2

--- AnimalStampede/AnimalStampede/Assets/Scripts/PlayerController.cs
+++ AnimalStampede/AnimalStampede/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
     public float speed = 10.0f;
     public float xRange = 15.0f;
 
+    public GameObject projectilePrefab;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -26,5 +28,10 @@ public class PlayerController : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
