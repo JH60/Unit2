53fd82fa77f0793d062b0220ba65c2e5f005ee53
Jaden Heimbach <heimbachj@roverkids.org>
2025-04-22 14:13:20 -0400

    Completed 2.4

--- AnimalStampede/AnimalStampede/Assets/Scripts/DestroyOutOfBounds.cs
+++ AnimalStampede/AnimalStampede/Assets/Scripts/DestroyOutOfBounds.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
-    private float topBound = 30;
+    private float topBound = 17;
     private float lowerBound = -20;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,11 +13,13 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //If an object goes past the players, view in the game, remove that object
         if (transform.position.z > topBound)
         {
             Destroy(gameObject);
         } else if (transform.position.z < lowerBound)
         {
+            Debug.Log("Game Over");
             Destroy(gameObject);
         }
     }
